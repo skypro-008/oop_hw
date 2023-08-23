8d71b91ce1be8e00801930acec35ff2ef6ffc7e6
kirillskyeng <kirill.netreba@skyeng.ru>
2023-08-23 13:50:24 +0300

    Add electronics-shop hw-2

--- src/item.py
+++ src/item.py
@@ -1,12 +1,47 @@
+import csv
+
+
 class Item:
     pay_rate = 1
     all = []
 
     def __init__(self, name: str, price: float, quantity=0):
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value: str):
+        if len(value) > 10:
+            raise Exception('Длина товара превышает 10 символов.')
+        self.__name = value
+
+    @classmethod
+    def from_csv(cls, filename):
+        with open(filename) as fp:
+            items = csv.DictReader(fp)
+            for item in items:
+                name = item['name']
+                price = float(item['price'])
+                if cls.is_integer(price):
+                    price = int(price)
+
+                quantity = int(item['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def is_integer(num) -> bool:
+        if isinstance(num, float):
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
 
     def calculate_total_price(self):
         return self.price * self.quantity
@@ -16,15 +51,23 @@ class Item:
 
 
 if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
-    print(item1.calculate_total_price())
-    print(item2.calculate_total_price())
-
-    Item.pay_rate = 0.8
-    item1.apply_discount()
+    Item.from_csv('items.csv')  # создание объектов из данных файла
+    print(len(Item.all))  # в файле 5 записей с данными по товарам
+    item1 = Item.all[0]
+    print(item1.name)
     print(item1.price)
-    print(item2.price)
+    print(item1.quantity)
 
-    print(Item.all)
+
+    # item1 = Item("Смартфон", 10000, 20)
+    # item2 = Item("Ноутбук", 20000, 5)
+    #
+    # print(item1.calculate_total_price())
+    # print(item2.calculate_total_price())
+    #
+    # Item.pay_rate = 0.8
+    # item1.apply_discount()
+    # print(item1.price)
+    # print(item2.price)
+    #
+    # print(Item.all)
--- tests/test_item.py
+++ tests/test_item.py
@@ -22,3 +22,35 @@ def test_apply_discount(item) -> None:
     Item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 800
+
+
+def test_name_norm_len(item):
+    item.name = 'Телефон'
+    assert item.name == 'Телефон'
+
+
+def test_name_too_long_len(item):
+    """Название товара слишком длинное"""
+    with pytest.raises(Exception):
+        item.name = 'ТелефонТелефонТелефон'
+
+
+def test_from_csv():
+    Item.all.clear()
+    Item.from_csv('tests/test_data/items.csv')
+    assert len(Item.all) == 2
+
+    assert Item.all[0].name == 'Товар 1'
+    assert Item.all[0].price == 10
+    assert Item.all[0].quantity == 5
+
+    assert Item.all[1].name == 'Товар 2'
+    assert Item.all[1].price == 20
+    assert Item.all[1].quantity == 15
+
+
+def test_is_integer():
+    assert Item.is_integer(10) is True
+    assert Item.is_integer(10.0) is True
+    assert Item.is_integer('10') is False
+    assert Item.is_integer(None) is False
